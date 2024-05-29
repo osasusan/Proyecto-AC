bede9c51fdbbe902c0fd5e7159cb01ec9fc3d399
osasusan <107266592+osasusan@users.noreply.github.com>
2024-05-29 19:14:41 +0200

    Initial Commit

---
+++ Proyecto AC/Proyecto_ACApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_ACApp.swift
+//  Proyecto AC
+//
+//  Created by Osasu sanchez on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_ACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
