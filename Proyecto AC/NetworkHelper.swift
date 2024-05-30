a45b5a5d6006283bd6b414572772c32efdaef5e5
osasusan <107266592+osasusan@users.noreply.github.com>
2024-05-30 21:14:10 +0200

    LOGIN-01-001
    fixed

--- Proyecto AC/NetworkHelper.swift
+++ Proyecto AC/NetworkHelper.swift
@@ -1,8 +1,44 @@
 //
-//  Networkhelper.swift
+//  NetworkHelper.swift
 //  Proyecto AC
 //
 //  Created by Osasu sanchez on 29/5/24.
 //
 
 import Foundation
+
+
+
+class NetworkHelper : NetworkProtocol {
+    enum RequestType : String{
+        case POST
+        case GET
+        case PUT
+        case DELETE
+    }
+    
+    private func requestApi(request : URLRequest) async throws -> (Data, URLResponse){
+        return try await URLSession.shared.data(for: request)
+    }
+
+    func requiesProvider (url : String ,type : RequestType ,params :[String: Any]? )async throws -> (Data,URLResponse){
+        
+        guard let urlNotNil = URL(string: url) else {
+            throw NetworkError.networkErrorEnum.invalidUrl
+        }
+        var request = URLRequest(url: urlNotNil)
+        request.httpMethod = type.rawValue
+        
+        
+        if let diction = params {
+            let data = try JSONSerialization.data(withJSONObject: diction, options: [])
+            request.httpBody = data
+            
+        }
+        
+        request.addValue("Bearer" , forHTTPHeaderField: "Authorization")
+        
+        request.addValue("application/json", forHTTPHeaderField: "Contetn-Type")
+        return try await requestApi(request: request)
+    }
+}
